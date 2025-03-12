ca4b85837def72a0118659947c55bb011d287c73
hinh2003 <vanhinh2003@gmail.com>
2025-03-12 20:47:31 +0700

    v2

--- app/Http/Controllers/Clients/ChapMovieController.php
+++ app/Http/Controllers/Clients/ChapMovieController.php
@@ -25,9 +25,15 @@ class ChapMovieController extends Controller
 
     public function showChapter(Movie $movie, $chapter)
     {
-        $chapters = Chap_movies::where('movie_id', $movie->id)->orderBy('name_chap', 'asc')->get();
+        $chapters = Chap_movies::where('movie_id', $movie->id)->get();
 
-        $selected_chapter = Chap_movies::findOrFail($chapter);
-        return view('Client.Chap_moive.videoMovies', compact('movie', 'chapters', 'selected_chapter'));
+        if ($chapters->isEmpty()) {
+            $default_chapter = null;
+            return view('Client.Chap_moive.index', compact('movie', 'chapters', 'default_chapter'))->with('message', 'Bộ phim chưa có tập nào.');
+        }
+
+        $default_chapter = $chapters->first();
+
+        return view('Client.Chap_moive.index', compact('movie', 'chapters', 'default_chapter'));
     }
 }
